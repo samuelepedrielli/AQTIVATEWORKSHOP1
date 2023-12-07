873561aee9c8bbe23b19d1d7bbb4e7664bba5883
Asmita29d <152626198+Asmita29d@users.noreply.github.com>
2023-12-07 16:58:12 +0100

    Add files via upload

--- job_DARDEL.sh
+++ job_DARDEL.sh
@@ -5,12 +5,18 @@
 #SBATCH -t 10:00
 #SBATCH --nodes=1
 #SBATCH -p shared
-#SBATCH -n <1>
+#SBATCH -n 1
 
-ml gnu7/7.2.0
+#ml gnu7/7.2.0
 ml PDC
+#ml likwid
+
 
 #Run your executable
 gfortran -o XY.x WXY_OG.f90
 
-srun -n 1 ./XY.x
+srun -n -1 ./XY.x
+#for value in 0.5 0.89 2.0; do
+ # echo $value > in_${value}.txt
+ # echo "in_${value}.txt" | srun -n 1 ./XY.x
+#done
